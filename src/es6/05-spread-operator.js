3e64dd633af1d82099f420cbc5786c7c095ae970
pipemalz <sinister012@gmail.com>
2022-12-25 20:06:04 -0500

    Spread operator y Rest parameters

--- src/es6/05-spread-operator.js
+++ src/es6/05-spread-operator.js
@@ -1,3 +1,4 @@
+//spread operator
 let kissEmoji = [..."👩‍❤️‍💋‍👩"]
 console.log(kissEmoji)
 
@@ -5,3 +6,30 @@ let familyEmoji = [..."👨‍👩‍👦‍👦"]
 console.log(familyEmoji)
 
 console.log('👩‍❤️‍💋‍👩');
+
+let person = {
+    name : 'Felipe',
+    age : '26',
+    profession : 'Web Developer'
+}
+
+let country = 'COL';
+
+let data = {id:12, ...person, country};
+
+console.log(data);
+
+let nums = [23,42,23,2,5,2365,2342,36,45,4567,43];
+
+let nums2 = [...nums, 1312, 123, 12312,124,12,12];
+
+console.log(nums2)
+
+//rest parameters
+
+function sumar(num, ...values){
+    console.log(values);
+    console.log(num + values[1]);
+}
+
+sumar(12,23,423,52,32,23,235,23);
